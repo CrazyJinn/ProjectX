57e12b4603d5fc2d1ff0a5895c2d5067b2b0dfcd
Jinn.W.Jing (g-mis.cnsh04.Newegg) 41330 <Jinn.W.Jing@newegg.com>
2017-05-10 15:45:12 +0800

    Add core project and mothed.

---
+++ Core/ProjectX.Core.Modeling/Class1.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ProjectX.Core.Model;
+
+namespace ProjectX.Core.Modeling
+{
+    public class Class1
+    {
+        public Input GetInput()
+        {
+            return new Input()
+            {
+                ParameterList = new List<Parameter>()
+                {
+                    new Parameter()
+                    {
+                        ParameterName="X",
+                        ParameterValueList = new List<int>()
+                        {
+                            1,
+                            2,
+                            3,
+                        }
+                    },
+                    new Parameter()
+                    {
+                        ParameterName="Y",
+                        ParameterValueList = new List<int>()
+                        {
+                            1,
+                            2,
+                            3,
+                        }
+                    },
+                },
+
+            };
+        }
+        
+    }
+}
