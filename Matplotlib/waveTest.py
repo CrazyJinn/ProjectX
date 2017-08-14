a5a41981cde2ab87ea66aea0b8940f853dc65db7
Jinn.W.Jing (g-mis.cnsh04.Newegg) 41330 <Jinn.W.Jing@newegg.com>
2017-08-14 09:38:01 +0800

    7

--- Matplotlib/waveTest.py
+++ Matplotlib/waveTest.py
@@ -10,17 +10,18 @@ filepath="E:\\z.wav"
 f = wave.open(filepath,'rb')
 params = f.getparams()
 nchannels, sampwidth, framerate, nframes = params[:4]
-strData = f.readframes(nframes)#读取音频，字符串格式
-waveData = np.fromstring(strData,dtype=np.int16)#将字符串转化为int
-waveData = waveData*1.0/(max(abs(waveData)))#wave幅值归一化
+strData = f.readframes(nframes)
+waveData = np.fromstring(strData,dtype=np.int16)
+waveData = waveData*1.0/(max(abs(waveData)))
 # plot the wave
 time = np.arange(0,nframes)*(1.0 / framerate)
 f.close()
 plt.plot(time,waveData)
+plt.axis([0.0,1.01,-1.0,1.0])
 plt.xlabel("Time(s)")
 plt.ylabel("Amplitude")
 plt.title("Single channel wavedata")
-plt.grid('on')#标尺，on：有，off:无。
+plt.grid('on')
 
 plt.legend()
 
