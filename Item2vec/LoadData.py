c5400e9fc2b8e26ced9b48eb8b59d4d156a2292c
unknown <crazyjinn@qq.com>
2020-11-01 23:19:53 +0800

    .

--- Item2vec/LoadData.py
+++ Item2vec/LoadData.py
@@ -1,24 +1,53 @@
 import csv
+import collections
+import numpy as np
 
-dic = {}
 
-with open('TestData\TestData.csv', 'r') as f:
-    reader = csv.reader(f)
-    for row in reader:
-        if(dic.__contains__(row[0])):
-            dic[row[0]].append(row[1])
-        else:
-            dic[row[0]] = [row[1]]
+vocabulary_size = 4
 
-# qqq = ["10-840-001", "10-840-002", "10-840-003",
-#        "10-840-004", "10-840-005", "10-840-006"]
 
-tempArr = []
+def LoadData(fileName):
+    with open(fileName, 'r') as f:
+        dic = dict()
+        itemDic = dict()
+        data = csv.reader(f)
+        for row in data:
+            if(dic.__contains__(row[0])):
+                dic[row[0]].append(row[1])
+            else:
+                dic[row[0]] = [row[1]]
+            if(itemDic.__contains__(row[1])):
+                itemDic[row[1]] += 1
+            else:
+                itemDic[row[1]] = 1
+        return (dic, itemDic)
 
-for key in dic.keys():
-    asd = dic[key].__len__() - 4
-    for i in range(asd):
-        tempArr.append(dic[key][i:i+5])
 
-for temp in tempArr:
-    print(temp)
+(dic, itemDic) = LoadData('TestData\TestData.csv')
+
+
+count = [['UNK', -1]]
+count.extend(collections.Counter(itemDic).most_common(vocabulary_size - 1))
+print(count)
+
+dictionary = dict()
+for item, _ in count:
+    dictionary[item] = len(dictionary)
+data = list()
+unk_count = 0
+for word in itemDic:
+    if word in dictionary:
+        index = dictionary[word]
+    else:
+        index = 0  # dictionary['UNK']
+        unk_count += 1
+    data.append(index)
+    count[0][1] = unk_count
+    reverse_dictionary = dict(zip(dictionary.values(), dictionary.keys()))
+
+print(reverse_dictionary)
+
+# for key in dic.keys():
+#     asd = dic[key].__len__() - 4
+#     for i in range(asd):
+#         tempArr.append(dic[key][i:i+5])
